ee3a33ff18bf769cf5ed926abfacb8561de1ca42
Marcel Nyembo <marcelmusuyu9@gmail.com>
2025-04-12 09:10:49 +0000

    continue resolving path issues

--- vite.config.js
+++ vite.config.js
@@ -13,7 +13,11 @@ export default defineConfig({
         register: resolve(__dirname, "src/account/register.html"),
 	application_details: resolve(__dirname, "src/job_process/application_details.html"),
 	application_grid: resolve(__dirname, "src/job_process/application_grid.html"),
-	
+	application_list: resolve(__dirname, "src/job_process/application_list.html"),
+	job_detail: resolve(__dirname, "src/job_process/job_detail.html"),
+        notification: resolve(__dirname, "src/job_process/notification.html"),
+        post_Job: resolve(__dirname, "src/job_process/post_Job.html"),
+        send_application: resolve(__dirname, "src/job_process/send_application.html"),
       },
     },
   },
